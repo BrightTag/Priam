0af56143b920490777c302f102db03cfccb00b39
Cody A. Ray <sites@codyaray.com>
2012-04-30 14:52:02 -0500

    [28088673:priam] Priam: interface for InstanceIdentity (SimpleDB) management

---
+++ src/main/java/com/netflix/priam/resources/PriamInstanceResource.java
@@ -0,0 +1,93 @@
+package com.netflix.priam.resources;
+
+import java.net.URI;
+import java.util.Map;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+import com.google.inject.Inject;
+import com.netflix.priam.IConfiguration;
+import com.netflix.priam.identity.IPriamInstanceFactory;
+import com.netflix.priam.identity.PriamInstance;
+
+/**
+ * Resource for manipulating priam instances.
+ */
+@Path("/v1/instances")
+@Produces(MediaType.TEXT_PLAIN)
+public class PriamInstanceResource
+{
+    private final IConfiguration config;
+    private final IPriamInstanceFactory factory;
+
+    @Inject
+    public PriamInstanceResource(IConfiguration config, IPriamInstanceFactory factory)
+    {
+        this.config = config;
+        this.factory = factory;
+    }
+
+    /**
+     * Get the list of all priam instances
+     * @return the list of all priam instances
+     */
+    @GET
+    public String getInstances()
+    {
+        StringBuilder response = new StringBuilder();
+        for (PriamInstance node : factory.getAllIds(config.getAppName()))
+        {
+            response.append(node.toString());
+            response.append("\n");
+        }
+        return response.toString();
+    }
+
+    /**
+     * Returns an individual priam instance by id
+     * 
+     * @param id the node id
+     * @return the priam instance
+     * @throws WebApplicationException(404) if no priam instance found with {@code id}
+     */
+    @GET
+    @Path("{id}")
+    public String getInstance(@PathParam("id") int id)
+    {
+        PriamInstance node = factory.getInstance(config.getAppName(), id);
+        if (node == null) {
+          throw notFound(String.format("No priam instance with id %s found", id));
+        }
+        return node.toString();
+    }
+
+    /**
+     * Creates a new instance with the given parameters
+     *
+     * @param id the node id
+     * @return Response (201) with vendor entity if the vendor was created
+     * @throws WebApplicationException (400) if the vendor already exists or invalid vendor data
+     */
+    @POST
+    public Response createInstance(@QueryParam("id") int id, @QueryParam("instanceID") String instanceID,
+        @QueryParam("hostname") String hostname, @QueryParam("ip") String ip, @QueryParam("rac") String rac,
+        @QueryParam("volumes") Map<String,Object> volumes, @QueryParam("payload") String payload)
+    {
+        factory.create(config.getAppName(), id, instanceID, hostname, ip, rac, volumes, payload);
+        URI uri = UriBuilder.fromPath("/{id}").build(id);
+        return Response.created(uri).build();
+    }
+
+    private WebApplicationException notFound(String message) {
+      return new WebApplicationException(Response.status(Response.Status.NOT_FOUND).entity(message).build());
+    }
+}
